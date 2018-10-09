d80a89a15da5a9ee901a228914ad989697e74c27
amytalbott <atalbott28@gmail.com>
2018-10-09 10:01:09 -0400

    added remove button

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+// Hide Remove buttons on page load
+// Make Remove buttons appear once currentAmout >= 1
+// Set the minimum value of currentAmount to 0
+
+window.onload = function() {
+  document.getElementById('remove-button').style.display = 'none';
+};
+
+
 const Counter = (props) => {
 
     let index = props.index;
@@ -8,7 +17,7 @@ const Counter = (props) => {
       <div className="counter">
         <button className="counter-action add" onClick={() => props.changeAmount(index, 1)}> Add </button>
         <span className="counter-score">{ props.currentAmount }</span>
-        <button className="counter-action remove" onClick={() => props.changeAmount(index, -1)}> Remove </button>
+        <button id="remove-button" className="counter-action remove" onClick={() => props.changeAmount(index, -1)}> Remove </button>
       </div>
     );
   }
